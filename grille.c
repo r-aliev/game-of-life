94653ed4ca8ba30ff48acb908e65b260e0ad5c88
r.aliyev <rashid.aliyev@etu.unistra.fr>
2020-02-21 09:26:14 +0100

    Add alloue_grille() and libere_grille().

--- grille.c
+++ grille.c
@@ -29,3 +29,44 @@ void copie_grille (grille gs, grille gd){
 	for (i=0; i<gs.nbl; ++i) for (j=0; j<gs.nbc; ++j) gd.cellules[i][j] = gs.cellules[i][j];
 	return;	
 }
+
+void alloue_grille (int l, int c, grille* g){
+	g->nbl = l;
+	g->nbc = c;
+	g->cellules =(int**) malloc(l * sizeof(int*));
+	for(int i = 0;i < l; i++){
+		g->cellules[i] =(int*) calloc(c , sizeof(int));
+	}
+}
+
+void libere_grille(grille* g){
+	for(int i = 0; i < g->nbl; i++){
+		free(g->cellules[i]);
+	}
+	free(g->cellules);
+}
+
+/*
+int main(int argc, char const *argv[])
+{
+	grille* g  = malloc(sizeof(grille));
+	init_grille_from_file("grilles/grille3.txt" , g);
+	printf("%d\n",g->cellules[5][5]);
+	
+	for(int i =0 ; i< g->nbc ; i++){
+		for(int j=0 ;j< g->nbl ; j++){
+			printf("%d ",g->cellules[i][j] );
+		}
+		printf("\n");
+	}
+	
+	grille gn  ;
+	alloue_grille(g->nbl , g->nbc , &gn);
+	copie_grille(*g , gn);
+	printf("%d\n",gn.cellules[5][5]);
+	
+	return 0;
+}
+*/
+
+
